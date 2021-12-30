572b9bbac9d4f5e011267584252a50c1c5f1defb
liwenmarcin <marcin.liwen@sohoshop.pl>
2021-12-30 11:26:18 +0100

    style i ikona

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -15,17 +15,19 @@ const Subscribe = () => {
   };
   return (
     <form onSubmit={submitHandler}>
-      <input
+        <div className="form-item">
+        <input
         type="email"
         id="email"
         name="email"
         pattern="[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}"
-        size="30"
         placeholder="Podaj swój e-mail"
         required
         onChange={(e) => setEmail(e.target.value)}
       />
       <input type="submit" value="Wyślij" />
+        </div>
+      
     </form>
   );
 };
--- src/pages/sucess.js
+++ src/pages/sucess.js
@@ -13,7 +13,7 @@ const IndexPage = ({data}) => {
         </div>
         <div className="hero-tex-container">
             <div className="hero-text-wrapper">
-            <h3>Skoro wiesz już o nas więcej - pozwól teraz pokazać co potrafimy.</h3>
+            <h2>Skoro wiesz już o nas więcej - pozwól teraz pokazać co potrafimy.</h2>
             <p>Umów się na BEZPŁATNE spotkanie wstępne - które pozwoli nam bliżej poznać Twoje potrzeby. Poniżej dostępny link - do naszego kalendarza online - gdzie możesz zabukować wygodny dla Ciebie termin.</p>
             <h4>UWAGA: jeśli zdecydujesz się na nasze usługi do końca tygodnia - to:</h4>
 
@@ -21,7 +21,7 @@ const IndexPage = ({data}) => {
   <li>Przeniesienie całej dokumentacji wraz z audytem wstępnym - otrzymasz GRATIS</li>
   <li>Pierwszy miesiąc obsługi GRATIS!</li>
 </ol>
-<a href="">Umów spotkanie</a>
+<a href="" className="btn">Umów spotkanie</a>
             </div>
             
         </div>
